f0b0575117c95067c3345c55eac3802feb500b0b
WitalyK <o211ntrrrtt@gmail.com>
2019-11-18 23:57:07 +0300

    5.2b

--- 5_2.py
+++ 5_2.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
-
-ip_m = input("Введите IP и Маску в формате: 10.1.1.0/24:")
-ip = ip_m.split('/')[0].split('.')
-mask = ip_m.split('/')[1]
-s = ['1']*int(mask)+['0']*(32-int(mask))
-ipp = '{:<8}  {:<8}  {:<8}  {:<8}'
-ipp1 = '{:08b}  {:08b}  {:08b}  {:08b}'
-print('Network:')
-ip[3]=0
-print(ipp.format(ip[0],ip[1],ip[2],ip[3]))
-print(ipp1.format(int(ip[0]),int(ip[1]),int(ip[2]),int(ip[3])))
-print('Mask:')
-print('/'+mask)
-ipp1 = '{}  {}  {}  {}'
-s1=','.join(s[0:8]).replace(',','')
-s2=','.join(s[8:16]).replace(',','')
-s3=','.join(s[16:24]).replace(',','')
-s4=','.join(s[24:]).replace(',','')
-print(ipp.format(int(s1,2),int(s2,2),int(s3,2),int(s4,2)))
-print(ipp1.format(s1,s2,s3,s4))
-
+from sys import argv
+try:
+    ip_m = argv[1]
+    #ip_m = input("Введите IP и Маску в формате: 10.1.1.0/24:")
+    ip = ip_m.split('/')[0].split('.')
+    mask = ip_m.split('/')[1]
+    s = ['1']*int(mask)+['0']*(32-int(mask))
+    ipp = '{:<8}  {:<8}  {:<8}  {:<8}'
+    ipp1 = '{:08b}  {:08b}  {:08b}  {:08b}'
+    print('Network:')
+    ip[3]=0
+    print(ipp.format(ip[0],ip[1],ip[2],ip[3]))
+    print(ipp1.format(int(ip[0]),int(ip[1]),int(ip[2]),int(ip[3])))
+    print('Mask:')
+    print('/'+mask)
+    ipp1 = '{}  {}  {}  {}'
+    s1=','.join(s[0:8]).replace(',','')
+    s2=','.join(s[8:16]).replace(',','')
+    s3=','.join(s[16:24]).replace(',','')
+    s4=','.join(s[24:]).replace(',','')
+    print(ipp.format(int(s1,2),int(s2,2),int(s3,2),int(s4,2)))
+    print(ipp1.format(s1,s2,s3,s4))
+except IndexError:
+    print('Неверный аргумент')
