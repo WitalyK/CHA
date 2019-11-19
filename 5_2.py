b7b88f9e9202746bad7c9b2541e169275c577f51
WitalyK <o211ntrrrtt@gmail.com>
2019-11-19 13:08:36 +0300

    5.2b

--- 5_2.py
+++ 5_2.py
@@ -5,7 +5,7 @@ try:
     #ip_m = input("Введите IP и Маску в формате: 10.1.1.0/24:")
     ip = ip_m.split('/')[0].split('.')
     mask = ip_m.split('/')[1]
-    s = ['1']*int(mask)+['0']*(32-int(mask))
+    s = '1'*int(mask)+'0'*(32-int(mask))
     ipp = '{:<8}  {:<8}  {:<8}  {:<8}'
     ipp1 = '{:08b}  {:08b}  {:08b}  {:08b}'
     print('Network:')
@@ -15,10 +15,10 @@ try:
     print('Mask:')
     print('/'+mask)
     ipp1 = '{}  {}  {}  {}'
-    s1=','.join(s[0:8]).replace(',','')
-    s2=','.join(s[8:16]).replace(',','')
-    s3=','.join(s[16:24]).replace(',','')
-    s4=','.join(s[24:]).replace(',','')
+    s1=s[0:8]
+    s2=s[8:16]
+    s3=s[16:24]
+    s4=s[24:]
     print(ipp.format(int(s1,2),int(s2,2),int(s3,2),int(s4,2)))
     print(ipp1.format(s1,s2,s3,s4))
 except IndexError:
