d59e208ce16bf0f3f521983de68ba9ffd0159a14
WitalyK <o211ntrrrtt@gmail.com>
2019-11-19 21:57:55 +0300

    6.2

--- 6_2.py
+++ 6_2.py
@@ -10,32 +10,33 @@
 'Неправильный IP-адрес'
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
-
-ip = input('Input IP Address: ')
-flag = False
-ipp = ip.split('.')
-if len(ipp) == 4:
-    for i in ipp:
-        if not i.isdigit():
-            flag = True
-            break
-        if not (0 <= int(i) <= 255):
-            flag = True
-            break
-    if not flag:
-        ip1 = int(ip.split('.')[0])
-        if 1 <= ip1 <= 223:
-            print('unicast')
-        elif 224 <= ip1 <= 239:
-            print('multicast')
-        elif ip == '255.255.255.255':
-            print('local broadcast')
-        elif ip == '0.0.0.0':
-            print('unassigned')
-        else:
-            print('unused')
-else:
-    flag = True
-if flag:
-    print('Неправильный IP-адрес')
+flag = True
+while flag:
+    ip = input('Input IP Address: ')
+    flag = False
+    ipp = ip.split('.')
+    if len(ipp) == 4:
+        for i in ipp:
+            if not i.isdigit():
+                flag = True
+                break
+            if not (0 <= int(i) <= 255):
+                flag = True
+                break
+        if not flag:
+            ip1 = int(ip.split('.')[0])
+            if 1 <= ip1 <= 223:
+                print('unicast')
+            elif 224 <= ip1 <= 239:
+                print('multicast')
+            elif ip == '255.255.255.255':
+                print('local broadcast')
+            elif ip == '0.0.0.0':
+                print('unassigned')
+            else:
+                print('unused')
+    else:
+        flag = True
+    if flag:
+        print('Неправильный IP-адрес!')
 
