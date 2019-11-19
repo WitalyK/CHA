326fbabd5ceb64bc632f59d668a842a29279aa6d
WitalyK <o211ntrrrtt@gmail.com>
2019-11-19 13:48:25 +0300

    5.3

--- 5_3.py
+++ 5_3.py
@@ -1,28 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from sys import argv
-
-'''
-Введите режим работы интерфейса (access/trunk): access
-Введите тип и номер интерфейса: Fa0/6
-Введите номер влан(ов): 3
-interface Fa0/6
-switchport mode access
-switchport access vlan 3
-switchport nonegotiate
-spanning-tree portfast
-spanning-tree bpduguard enable
-Пример выполнения скрипта, при выборе режима trunk:
-$ python task_5_3.py
-Введите режим работы интерфейса (access/trunk): trunk
-Введите тип и номер интерфейса: Fa0/7
-Введите номер влан(ов): 2,3,4,5
-interface Fa0/7
-switchport trunk encapsulation dot1q
-switchport mode trunk
-switchport trunk allowed vlan 2,3,4,5
-'''
-
 access_template = [
     'switchport mode access', 'switchport access vlan {}',
     'switchport nonegotiate', 'spanning-tree portfast',
