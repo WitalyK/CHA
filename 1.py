67d5313e88d3ece6adbe02d38a0b912b03b828ba
onair <C:\Users\onair\AppData\Roaming\The Bat!>
2019-12-01 20:12:21 +0300

    15ready_17prepared

--- 1.py
+++ 1.py
@@ -1,10 +1,12 @@
 # -*- config: utf8 -*-
 import requests
+import json
 
 adresok = 'сыктывкар карла маркса 197'
 
 r = requests.get('http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=RU&addressLine={}&key=AjpbWY8LT5rqIET70iVolfI0biR-u9P5jg5hSuAllOPS77_mHdD7EXMImvKlYlPT'.format(adresok))
 d = r.json()
+
 print('Геоданные: ', d['resourceSets'][0]['resources'][0]['point']['coordinates'])
 print('Адресок:')
 for key, value in d['resourceSets'][0]['resources'][0]['address'].items():
