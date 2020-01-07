b9e95d637a02f5a2a0ecc4e094cfc8178384496d
WitalyK <o211ntrrrtt@gmail.com>
2020-01-07 22:01:50 +0300

    26

--- 27_oop_inheritance/task_27_1.py
+++ 27_oop_inheritance/task_27_1.py
@@ -17,12 +17,19 @@ In [4]: r1.send_show_command('sh ip int br')
 Out[4]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
 
 '''
+class CiscoSSH(BaseSSH):
+    def __init__(self):
+        super().__init__(self, **device_params)
+		self.ssh.enable()
 
-device_params = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.100.1',
-    'username': 'cisco',
-    'password': 'cisco',
-    'secret': 'cisco'
-}
-
+#don't run on import
+if __name__ == "__main__":
+    device_params = {
+        'device_type': 'cisco_ios',
+        'ip': '10.111.111.11',
+        'username': 'admin',
+        'password': 'cisco',
+        'secret': 'cisco'
+    }
+    r1 = CiscoSSH(**device_params)
+    print(r1.send_show_command('sh ip int br'))
