6c15ce89811caad4342e27fb2661e07bc4814efa
WitalyK <o211ntrrrtt@gmail.com>
2020-02-08 19:56:24 +0300

    all

--- logging/1.py
+++ logging/1.py
@@ -1,7 +1,7 @@
 import logging
 
-logging.basicConfig(filename='mylog.log', level=logging.DEBUG)
+logging.basicConfig(filename='mylog.log', level=logging.WARNING)
 
-logging.debug('Сообщение уровня debug')
+logging.debug('Сообщение уровня debug:\n%s', str(globals()))
 logging.info('Сообщение уровня info')
 logging.warning('Сообщение уровня warning')
---
+++ logging/2.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger('My Script')
+
+## messages
+logger.debug('Сообщение уровня debug')
+logger.info('Сообщение уровня info')
+logger.warning('Сообщение уровня warning')
